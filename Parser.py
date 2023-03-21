5728efaf67b725620cee7f051da69b7a7e34db47
DubitskiyKirill <125906191+DubitskiyKirill@users.noreply.github.com>
2023-03-21 14:17:52 +0600

    Add files via upload

--- Parser.py
+++ Parser.py
@@ -6,11 +6,12 @@ def parse():
     page = requests.get(url) # отправляем запрос методом Get на данный адрес и получаем ответ в переменную
     print(page.status_code) # смотрим ответ
     soup = BeautifulSoup(page.text, "html.parser") # передаем страницу в bs4
-
+    f = open('file.txt', 'w', encoding='utf-8')
     allCars = soup.findAll('div', class_='css-17lk78h e3f4v4l2')#марка и модель автомобиля, год выпуска
     #allCars = soup.findAll('div', class_='css-13ocj84 e1icyw250')  # всё вышеперечисленное, плюс краткое описание
     description = ' '
     for data in allCars: # проходим циклом по содержимому контейнера
         if data.find('span'): # находим тег <span>
+            f.write(f"{data.text}\n")
             description = data.text # записываем в переменную содержание тега
         print(description)
